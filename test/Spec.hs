665ff3e52b45f2b00ae9e451b42fc2530d14ac99
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-12-18 15:39:26 -0500

    Update the test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,20 +1,18 @@
 import Chess
 import Lib (generateGame, heuristic, randomCommand)
 
-
 main :: IO ()
 main = do
   let game = generateGame
-
-  let (Game board _ _ _) = game
-  print $ piecesOf White board
-
   let nextState g = case randomCommand g of
         Update game command -> game
 
-  let loop20 = iterate nextState game !! 100
+  -- game state after 100 moves.
+  let loop100 = iterate nextState game !! 100
 
-  let score = heuristic loop20
-  let (Game board _ _ _) = loop20
-  print $ piecesOf White board
-  print score
+  let score = heuristic loop100
+  --   let (Game board _ _ _) = loop20
+  --   print $ piecesOf White board
+
+  -- this is actually deterministic so score should not be zero.
+  putStrLn $ if score /= 0 then "OK" else "FAIL!"
