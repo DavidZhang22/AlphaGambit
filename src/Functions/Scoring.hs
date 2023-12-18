06927589ac7d59253e8cec8b8790fa6edbe1be54
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-12-18 16:20:54 -0500

    Added comment

--- src/Functions/Scoring.hs
+++ src/Functions/Scoring.hs
@@ -4,6 +4,7 @@ module Functions.Scoring (heuristic) where
 
 import Chess
 
+-- Heuristic returns a positive score if white is winning, and a negative score if black is winning.
 heuristic :: Game -> Int
 heuristic (Game board _ _ _) = values (piecesOf White board) (piecesOf Black board)
   where
