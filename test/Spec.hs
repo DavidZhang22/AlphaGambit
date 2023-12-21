8584b415c00f421aeb1fda5d35ef07dabc4b9980
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-12-20 22:41:00 -0500

    Update test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,33 +1,34 @@
 import Chess
-import Data.List (intercalate)
-import Lib (generateGame, heuristic, nextStates, randomCommand, minimax)
+import Lib (generateGame, heuristic, minimax, nextStates, randomCommand)
 
 main :: IO ()
 main = do
   -- TEST 1: Heuristic is non-zero when pieces change.
 
   let game = generateGame
-  let nextState g = case randomCommand g of
-        Update game command -> game
+  let nextState g =
+        let Update game1 _ = randomCommand g
+         in game1
 
   -- game state after 100 moves.
   let loop100 = iterate nextState game !! 100
-
   let score = heuristic loop100
-  --   let (Game board _ _ _) = loop20
-  --   print $ piecesOf White board
 
   -- this is actually deterministic so score should not be zero.
-  putStrLn $ if score /= 0 then "OK" else "FAIL!"
+  putStrLn "TEST 1: Heuristic is non-zero when pieces change."
   putStrLn $ "Score: " ++ show score
+  putStrLn $ if score /= 0 then "OK" else "FAIL!"
 
   -- TEST 2: Next States function works
---   let potentialNextStates = nextStates loop100
---   putStrLn $ intercalate "\n" $ map (\(update) -> show update.command) potentialNextStates
+  putStrLn "TEST 2: Next States function finds >0 states"
+  let potentialNextStates = nextStates loop100
+  let numStates = length potentialNextStates
+  putStrLn $ "Found " ++ show (length potentialNextStates) ++ " potential updates"
+  putStrLn $ if numStates /= 0 then "OK" else "FAIL!"
 
   -- Test 3: MinMax function
-  let futureUpdate = minimax game 5 game.activePlayer
+  putStrLn "TEST 3: Min Max function runs successfully."
+  let futureUpdate = minimax game 4 game.activePlayer
   putStrLn "Minimax update:"
-  putStrLn $ show futureUpdate.command
-
-
+  print futureUpdate.command
+  putStrLn "OK"
